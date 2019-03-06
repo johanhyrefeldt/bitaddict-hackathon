ff438700acb394f6c9c684f521814a6433503e97
Mickel <hoangmicke95@gmail.com>
2019-03-06 21:13:32 +0100

    dawdwa

--- myBot/bothub/bot.py
+++ myBot/bothub/bot.py
@@ -1,11 +1,90 @@
 # -*- coding: utf-8 -*-
-
+#!/usr/bin/env python
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 from bothub_client.bot import BaseBot
 from bothub_client.decorators import channel
 
+#%%
+
+import sys
+import time
+import random
+import string
+import hashlib
+import http.client
+import json
+from datetime import datetime
+import requests
+
+def dag_api():
+    datet = datetime.now() 
+    day = str(datet.day)
+    month = str(datet.month)
+    year = str(datet.year)
+
+    url_day = ('http://api.dryg.net/dagar/v2.1/'+year+'/'+month+'/'+day)
+
+    print('date url:',url_day)
+    
+    resp = requests.get(url_day)
+    #print(resp.json())
+    response_json = resp.json()['dagar'][0]['namnsdag']
+    print(response_json)
+    
+    return response_json
+
+def hitta_api(plats='goteborg'):
+    namsdag = dag_api()[0]
+
+    # Get url from first argument
+    url = '/publicsearch/v1/persons?what={}&where={}&range.from=1&range.to=50'.format(namnsdag,plats)
+
+    # Authentication parameters
+    caller_id = "Lag1"
+    time = str(int(time.time()))
+    key =  "cq9MxlplYqbiAtiPZctvhNqlWaHRsaEUgSZj57uk"
+    random = "".join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(16))
+
+    # Create the hashed string
+    string_to_hash = caller_id + time + key + random
+    hashed_string = hashlib.sha1(string_to_hash.encode('utf-8')).hexdigest()
+
+    # The http headers
+    headers = {
+       "X-Hitta-CallerId": caller_id,
+       "X-Hitta-Time": time,
+       "X-Hitta-Random": random,
+       "X-Hitta-Hash": hashed_string
+    }
+
+    # Make the call
+    conn = http.client.HTTPSConnection("api.hitta.se")
+    conn.request("GET", url, "", headers)
+    resp = conn.getresponse()
+
+    # Print response
+    #print("resp.status\n{}\nresp.reason{}".format(resp.status, resp.reason))
+    json_response = json.loads(resp.read())
+    #print(json.dumps(json_response, indent=4, separators=(',', ': ')))
+
+    #%%
+    for el in json_response['result']['persons']['person']:
+        name = str(el['displayName'])
+        first_name = name.split(" ")[0]
+        if first_name.lower() == 'nils':
+            message = "Här är dagens namnsdagsbarn :) \n"
+            try:
+                phone = el['phone'][0]
+                phone_number = phone['callTo']
+                message = message + "Namn: {} telefonnummer {}\n".format(el['displayName'], phone_number)
+            except KeyError:
+                message = message + "Namn: {} Ledsen! Inget telefonnummer\n".format(el['displayName'])
+
+    return message
+
 def get_namedate():
+    hitta_api()
     str = "Idag har %s namnsdag! \n Gratta hen med nummer: %s" % ("Anders", "073 917 48 46")
     return str
 
@@ -59,6 +138,7 @@ class Bot(BaseBot):
            "raw_data": <unmodified data itself webhook received>
         }
         """
-        self.send_message(get_namedate())
+        msg = hitta_api(event["content"])
+        self.send_message(msg)
 
     
